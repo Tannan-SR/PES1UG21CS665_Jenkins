dded0c4d7ab0c640fc4bd77ef49949c4899ec1f9
Tannan-SR <114357366+Tannan-SR@users.noreply.github.com>
2024-03-07 17:59:33 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@ using namespace std;
 int main(){
     int i=1;
     int a[]={0,1,2,3,4,5,6,7};
-    for(int i=1;i<8;i++){
-        a[i] *=2;
+    for(int i=1;i<8;i++)
+    {
+        a[i] += 2;
         cout<<a[i]<<endl;
     }
-
+    return 0;
 }

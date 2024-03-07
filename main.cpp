bd7ffe72a4c4a44f2c6743e9e368e7ef70f4b03b
Tannan-SR <114357366+Tannan-SR@users.noreply.github.com>
2024-03-07 18:03:12 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int main() {
     int a[] = {0, 1, 2, 3, 4, 5, 6, 7};
     for(int i = 1; i < 8; i++) {
         a[i] += 2;
-        cout << a[i] << endl;
+        cout << a[i] << endl
     }
     return 0;
 }

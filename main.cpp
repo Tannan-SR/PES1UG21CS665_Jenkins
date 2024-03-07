96edfa9eece2f22c8e9dce8041f01614a68a5097
Tannan-SR <114357366+Tannan-SR@users.noreply.github.com>
2024-03-07 17:55:01 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i=1;
+    int a[]={0,1,2,3,4,5,6,7};
+    for(int i=1;i<8;i++){
+        a[i] *=2;
+        cout<<a[i]<<endl;
+    }
+
+}
